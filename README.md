33a672bb15346fa02f79154e9ce19cfec877dd9e
AndikaPurnamaPutra <andikapratamaputra849@gmail.com>
2023-09-26 17:29:59 +0700

    --build: section header, section product, section other product, section subscription

---
+++ README.md
@@ -0,0 +1,11 @@
+# Theme Visual Tap
+
+## How to use
+
+```sh
+npm install
+npm start
+```
+<!-- ## Cover
+
+<img src="Cover.png" alt="cover"> -->
---
+++ assets/js/main.js
@@ -0,0 +1,54 @@
+console.log("hallo");
+
+// Navbar Responsive
+
+// const navbarToggler = document.querySelector(".nav-btn-responsive");
+
+// navbarToggler.addEventListener("click", function () {
+//     document.querySelector(".nav-menu-responsive").classList.toggle("active");
+// });
+
+// logic respondsive
+
+// const btnRes = document.querySelector(".nav-btn-responsive");
+// const menu = document.querySelector(".nav-menu");
+
+// btnRes.addEventListener("click", () => {
+//     menu.classList.toggle("active");
+//     btnRes.classList.toggle("cross");
+// });
+
+// logic copy clipboard
+
+const copyButtonLabel = "Components";
+
+// use a class selector if available
+let blocks = document.querySelectorAll("pre");
+
+blocks.forEach((block) => {
+    // only add button if browser supports Clipboard API
+    if (navigator.clipboard) {
+        let button = document.createElement("button");
+
+        button.innerText = copyButtonLabel;
+        block.appendChild(button);
+
+        button.addEventListener("click", async () => {
+            await copyCode(block, button);
+        });
+    }
+});
+
+async function copyCode(block, button) {
+    let code = block.querySelector("code");
+    let text = code.innerText;
+
+    await navigator.clipboard.writeText(text);
+
+    // visual feedback that task is completed
+    button.innerText = "Code Copied!";
+
+    setTimeout(() => {
+        button.innerText = copyButtonLabel;
+    }, 700);
+}
